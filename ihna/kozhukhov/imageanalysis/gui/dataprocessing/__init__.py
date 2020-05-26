37fe6c2347e155f85bf609c689f35929a8fcb047
serik1987 <serik1987@gmail.com>
2020-05-26 03:55:11 +0300

    "Use this map for ROI selection" was implemented

--- ihna/kozhukhov/imageanalysis/gui/dataprocessing/__init__.py
+++ ihna/kozhukhov/imageanalysis/gui/dataprocessing/__init__.py
@@ -5,6 +5,7 @@ from .MapAverageDlg import MapAverageDlg
 from .mapfillerdlg import MapFillerDlg
 from .tracefilter import TraceFilterDlg
 from .tracepower import TracePower
+from .setmainprocessor import SetMainProcessor
 
 
 def get_data_processors(parent):
@@ -16,7 +17,8 @@ def get_data_processors(parent):
         processors.update({
             "Map filter": SpatialFilterDlg,
             "Compute average value": MapAverageDlg,
-            "New map with predefined valued": MapFillerDlg
+            "New map with predefined valued": MapFillerDlg,
+            "Set ROI using this map": SetMainProcessor
         })
 
     if isinstance(parent, TraceResultListDlg):
---
+++ ihna/kozhukhov/imageanalysis/gui/dataprocessing/setmainprocessor.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8
+
+import wx
+from .datatonumberprocessor import DataToNumberProcessor
+
+
+class SetMainProcessor(DataToNumberProcessor):
+
+    __is_main_box = None
+
+    def _get_processor_title(self):
+        return "Map average value"
+
+    def _get_default_minor_name(self):
+        return "is_main"
+
+    def _place_additional_options(self, parent):
+        self.__is_main_box = wx.CheckBox(parent, label="Use this map for ROI selection")
+        input_data = self._input_data
+        if 'is_main' in input_data.get_features() and input_data.get_features()['is_main'] == "yes":
+            self.__is_main_box.SetValue(True)
+        else:
+            self.__is_main_box.SetValue(False)
+
+        return self.__is_main_box
+
+    def _process(self):
+        if self.__is_main_box.IsChecked():
+            self._output_data = "yes"
+        else:
+            self._output_data = "no"
+
+    def _print_output_value(self):
+        if self._output_data == "yes":
+            return "This map will be used for ROI selection"
+        else:
+            return "This map will not be used for ROI selection"
